30e026f06e0009d6fd957ae83ea8b6429c72e9bb
ogabrielsilvaa <ogabrieusiuva@gmail.com>
2026-01-05 11:43:18 -0300

    Adição das rotas de páginas já construídas.

--- estante-virtual-frontend/src/App.tsx
+++ estante-virtual-frontend/src/App.tsx
@@ -1,12 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './context/authContext'
 import { Login } from './pages/login';
+import { Register } from './pages/register';
+import { MyShelf } from './pages/myShelf';
 
 function App() {
 
   return (
-    <AuthProvider>
-      <Login />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path='/' element={<Navigate to='/login'/>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+
+          <Route path='/myShelf' element={<MyShelf />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
